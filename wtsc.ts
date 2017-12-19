7b61da61cc4c26a290d1f8e9a941f298768776c3
Christophe Eymard <christophe.eymard@gmail.com>
2017-12-20 00:40:55 +0100

    first version

---
+++ wtsc.ts
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk'
+import {spawn} from 'child_process'
+
+const args = process.argv.slice(2)
+const log = (s: string) => process.stdout.write(s)
+
+const h = (n: number) => chalk.hsl(n, 100, 60)
+const c_file = h(0)
+const c_line = h(130).bold
+const c_end = chalk.rgb(22, 45, 43)
+const c_prop = chalk.hsl(80, 100, 10)
+const c_type = chalk.hsl(170, 100, 10)
+const basic = chalk.rgb(80, 80, 80)
+
+type ReplacerFn = (matches: string[]) => string
+
+const mp = new Map<RegExp, ReplacerFn>()
+
+mp.set(/(\s|\n)*^(.*?Compilation complete.*?)$(\s|\n)*/mg, ([sp, line]) => {
+  return c_end.bold(`\n  ${line}\n`)
+})
+
+mp.set(/Property '([^']*)'/ig, ([match]) => {
+  return `Property ${c_prop(match)}`
+})
+
+mp.set(/type '([^']*)'/ig, ([match]) => {
+  return `type ${c_type(match)}`
+})
+
+mp.set(/Error TS\d+: /gi, () => '')
+
+
+// Files
+mp.set(/^([^\(\n]+\/)?([^\(\n)]+)\((\d+),(\d+)\):/gm, ([path, file, line, col]) => {
+  return `${c_file(path)}${c_file.bold(file)} ${c_line(line)}:\n`
+})
+
+var tsc = spawn('tsc', args)
+
+tsc.stdout.on('data', (_out: Buffer) => {
+  var out = _out.toString('utf-8')
+  mp.forEach((fn, reg) => {
+    out = out.replace(reg, (str, ...matches) => {
+      return fn(matches)
+    })
+  })
+  log(basic(out))
+})
+
+tsc.stderr.on('data', err => {
+  // log(`err: ${err}`)
+})
+
+tsc.on('close', (code, signal) => {
+  // log(`exited with ${code}, ${signal}`)
+})
