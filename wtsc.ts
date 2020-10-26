a1039bbb94bb0282a48d782da9e1773c8263e4f5
Christophe Eymard <christophe.eymard@sales-way.com>
2020-10-26 15:29:15 +0100

    trigger a command when a build or check finishes

--- wtsc.ts
+++ wtsc.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import _chalk from 'chalk'
+import { ChildProcess, spawn } from 'child_process'
 
 const log = (s: string) => {
   process.stdout.write(s.replace('\x1Bc', ''))
@@ -62,13 +63,60 @@ mp.set(/^([^\(\n]+\/)?([^\(\n)]+)\((\d+),(\d+)\):/gm, (path, file, line, col) =>
   return `${path ? c_file(path) : ''}${c_file.bold(file)} ${c_line(line)}: `
 })
 
+// Command to be relaunched
+const command = process.argv.slice(2)
+const should_run_command = command.length > 0
+var running_process: null | ChildProcess = null
+var should_rerun = false
+
+const runcmd = command.length === 0 ? () => {} : debounce(() => {
+  if (running_process != null) {
+    should_rerun = true
+    return
+  }
+  const [exe, ...args] = command
+  running_process = spawn(exe, args, {
+    stdio: 'inherit'
+  })
+  running_process.on('exit', () => {
+    running_process = null
+    if (should_rerun) runcmd()
+    should_rerun = false
+  })
+}, 50)
+
 
 process.stdin.on('data', (_out: Buffer) => {
   var out = _out.toString('utf-8')
   mp.forEach((fn, reg) => {
-    out = out.replace(reg, (str, ...matches) => {
+    out = out.replace(reg, (_, ...matches) => {
       return fn(...matches)
     })
   })
+
+  if (should_run_command && out.includes('Watching for file changes.')) {
+    runcmd()
+  }
+
   log(basic(out.trim()) + '\n')
 })
+
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func: Function, wait: number, immediate: boolean = false) {
+	var timeout: NodeJS.Timer | null;
+	return function(this: any) {
+		var context = this, args = Array.from(arguments);
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var call_now = immediate && !timeout;
+		if (timeout != null) clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (call_now) func.apply(context, args);
+	};
+};
